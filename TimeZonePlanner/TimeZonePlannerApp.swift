a7c9e1f9d1a2ea869e8df51168a88a87a499488a
Sergei Kriukov <admtransmedia@icloud.com>
2022-06-01 12:22:06 +0600

    Initial Commit

---
+++ TimeZonePlanner/TimeZonePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZonePlannerApp.swift
+//  TimeZonePlanner
+//
+//  Created by Sergei Kriukov on 01.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeZonePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
